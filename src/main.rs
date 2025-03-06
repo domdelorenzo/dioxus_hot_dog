985edfc884628549bcf1fb3c8427f0112a3b7e40
Dominick DeLorenzo <49847428+domdelorenzo@users.noreply.github.com>
2025-03-06 13:15:34 -0600

    everything through fetching data

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,48 @@ fn main() {
     dioxus::launch(App);
 }
 
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
 #[component]
 fn App() -> Element {
     rsx! {
         document::Stylesheet { href: CSS }
+        Title {}
+        DogView {}
+    }
+}
 
+#[component]
+fn Title() -> Element {
+    rsx! {
         div { id: "title",
             h1 { "🌭 HotDog!"}
-        } 
-        div { id: "dogview",
-            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
-        }
-        div {
-            id: "buttons",
-            button { id:"skip", "skip" }
-            button { id:"save", "save" }
         }
     }
 }
 
+#[component]
+fn DogView() -> Element {
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+
+    rsx! {
+        div { id: "dogview",
+            img { src: img_src.cloned().unwrap_or_default() }
+        }
+        div { id: "buttons",
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
+            button { onclick: move |_| img_src.restart(), id: "save", "save!" }
+        }
+    }
+}
