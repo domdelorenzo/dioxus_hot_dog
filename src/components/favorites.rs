b724a9faf83854fbc5728f30fd8a86bb1eaf25ad
Dominick DeLorenzo <49847428+domdelorenzo@users.noreply.github.com>
2025-03-19 16:12:35 -0500

    Removes commented code

--- src/components/favorites.rs
+++ src/components/favorites.rs
@@ -4,13 +4,12 @@ use dioxus::prelude::*;
 pub fn Favorites() -> Element {
     // Create a pending resource that resolves to the list of dogs from the backend
     // Wait for the favorites list to resolve with `.suspend()`
+    // However, this won't allow you to call favorites.restart() to update the list on deletion 
     // let favorites: MappedSignal<Result<Vec<(usize, String)>, ServerFnError>> = use_resource(crate::backend::list_dogs).suspend()?;
 
-
-    // let mut favorite_resource = use_resource(crate::backend::list_dogs);
-    // let favorites = favorite_resource.suspend()?;
-
+    // make favorites mutable and remove suspend so that we can call favorites.restart() to update the list on deletion
     let mut favorites = use_resource(crate::backend::list_dogs);
+    // assign favorites to a signal and suspend there to pass it to the for loop
     let favorites_signal = favorites.suspend()?;
     rsx! {
         div { id: "favorites",
